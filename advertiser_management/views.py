6ff834d0576160d2926027b1ea25bfff5a8d69f5
xmhpx <mhppaydar@gmail.com>
2021-10-08 20:47:45 +0330

    added placeholder views and urls

--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("smt smt smt")
+    return HttpResponse("ad ad ad ad")
+
+
+def inc_clicks(request, ad):
+    return HttpResponse("inc_clicks %s" % ad)
+
+
+def create_ad(request):
+    return HttpResponse("some form to create new ad")
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('advertiser_management/', include('advertiser_management.urls')),
+    path('', include('advertiser_management.urls')),
 ]

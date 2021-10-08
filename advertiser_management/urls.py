70d381229543a0ac3c5358a3b191c1225fb5593a
xmhpx <mhppaydar@gmail.com>
2021-10-08 21:28:43 +0330

    renamed views.inc_clicks to views.click and fixed functionality

--- advertiser_management/urls.py
+++ advertiser_management/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('inc_clicks/', views.inc_clicks, name='inc_clicks'),
+    path('click/<int:ad_id>/', views.click, name='click'),
     path('create_ad/', views.create_ad, name='create_ad'),
 ]
--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 
@@ -7,11 +7,22 @@ from .models import Ad, Advertiser
 def home(request):
     advertisers = Advertiser.objects.order_by('-clicks')
 
+    for advertiser in advertisers:
+        for ad in advertiser.ad_set.all():
+            ad.views += 1
+            ad.save()
+
     return render(request, 'ads.html', {'advertisers': advertisers})
 
 
-def inc_clicks(request, ad):
-    return HttpResponse("inc_clicks %s" % ad)
+def click(request, ad_id):
+    ad = get_object_or_404(Ad, pk=ad_id)
+    ad.clicks += 1
+    ad.save()
+    output = "You clicked on %s" % ad
+    clcks = " now it has %s clicks" % ad.clicks
+    views = " and %s views" % ad.views
+    return HttpResponse(output + clcks + views)
 
 
 def create_ad(request):

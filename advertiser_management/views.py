04048d0916e6d54463bce058bcefa4a88116f47f
xmhpx <mhppaydar@gmail.com>
2021-10-08 21:40:15 +0330

    added redirection in click view

--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Ad, Advertiser
 
@@ -19,10 +19,10 @@ def click(request, ad_id):
     ad = get_object_or_404(Ad, pk=ad_id)
     ad.clicks += 1
     ad.save()
-    output = "You clicked on %s" % ad
-    clcks = " now it has %s clicks" % ad.clicks
-    views = " and %s views" % ad.views
-    return HttpResponse(output + clcks + views)
+    # output = "You clicked on %s" % ad
+    # clcks = " now it has %s clicks" % ad.clicks
+    # views = " and %s views" % ad.views
+    return HttpResponseRedirect(ad.link)
 
 
 def create_ad(request):
